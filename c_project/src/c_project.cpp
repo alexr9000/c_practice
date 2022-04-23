135a713fd355dd751368404a4250da54252e1242
Alex Ramirez <alexr@skycatch.com>
2022-04-23 02:30:02 -0700

    First

---
+++ c_project/src/c_project.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : c_project.cpp
+// Author      : Alex Ramirez
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
