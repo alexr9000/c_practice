0daa0ef32707fbe2bccaa3596e4bb4f2d304a2fe
Alex Ramirez <alexr@skycatch.com>
2022-04-24 22:22:36 -0700

    Overloaded assignment operator

--- c_operator_overloading/src/assignment.cpp
+++ c_operator_overloading/src/assignment.cpp
@@ -20,9 +20,39 @@ public:
 	void print() {
 		cout << id << ": " << name << endl;
 	}
+
+	const oTest &operator=(const oTest &other) {
+		cout << "Assignment" << endl;
+		id = other.id;
+		name = other.name;
+
+		return *this;
+	}
+
+	oTest(const oTest &other) {
+		cout << "Copy constructor running" << endl;
+		//id = other.id;
+		//name = other.name;
+		*this = other;
+	}
 };
 
 int main() {
+
+	oTest test1(10, "Mike");
+	test1.print();
+
+	oTest test2(20, "Bob");
+
+	test2 = test1;
+	test2.print();
+
+	oTest test3;
+	test3.operator=(test2);
+	test3.print();
+
+	oTest test4 = test1;
+	test4.print();
 
 	return 0;
 }
